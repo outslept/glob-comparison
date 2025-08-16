5faa51b30fd20f510d0de47f30b600577a72dac4
outslept <135520429+outslept@users.noreply.github.com>
2025-08-16 04:31:07 +0300

    chore: make sure os dependant snapshots are not overriden

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
   test: {
+    resolveSnapshotPath: (testPath, snapExtension) => {
+      const platform = process.platform === 'win32' ? 'win32' : 'linux';
+      const dir = path.dirname(testPath);
+      const filename = path.basename(testPath, path.extname(testPath));
+      return path.join(dir, '__snapshots__', `${filename}.${platform}${snapExtension}`);
+    },
     projects: [
       {
         test: {
