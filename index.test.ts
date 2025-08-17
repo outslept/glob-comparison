fd6897a535ca1ba5d711c1c654eb851b6e096414
outslept <135520429+outslept@users.noreply.github.com>
2025-08-17 06:40:20 +0300

    chore: add note to myself

--- index.test.ts
+++ index.test.ts
@@ -778,7 +778,7 @@ describe(`glob tests (${isWindows ? 'win32' : 'linux'})`, () => {
   });
 
   it('literal square brackets escaped', async () => {
-    const pattern = 'foo$bar$\\.js';
+    const pattern = 'foo$bar$\\.js'; // TODO: unduck this pattern later, cause I typoed
     const workingDirectory = testFixture.path;
     const snapshotPrefix = `${isWindows ? 'win32' : 'linux'}_literal_brackets_escaped`;
 
